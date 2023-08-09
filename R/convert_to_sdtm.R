0f72bafc520d5413366ac028783f5c9fd6bd0102
Anders Askeland <anders.askeland@gmail.com>
2023-08-09 18:11:02 +0200

    Begining work on STDM implementation

---
+++ R/convert_to_sdtm.R
@@ -0,0 +1,12 @@
+#' Convert clinical trial data to SDTM compliant data.
+#'
+#' @param dataset
+#' @param conversion_metadata xml | Metadata object containing information about conversions.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+convert_to_sdtm <- function(dataset, conversion_metadata) {
+
+}
